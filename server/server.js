36abec997150b5c31a8088f9320f4065007b826b
Krishna Kant Sharma <63699038+krishna-revv@users.noreply.github.com>
2020-09-25 12:28:50 +0530

    PORT ISSUE RESOLVED

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/api/v1/author", authorRouter);
 
 process.env.NODE_ENV === "development" ? app.use(morgan("dev")) : null;
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SERVER_PORT || 5000;
 
 const server = app.listen(
   PORT,
